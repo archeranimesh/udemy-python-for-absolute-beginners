9fc931633fbea37ae2c8398392f601170d25a70d
Animesh Bhadra <animeshb@archerimagine.com>
2023-06-16 16:47:24 +0530

    📦 NEW: Function Parameter.

---
+++ SRC/Section_07-Functions/param.py
@@ -0,0 +1,8 @@
+def output_welcome_text(name):
+    """Output welcome text with provided name"""
+    print(f"Welcome {name}")
+
+
+output_welcome_text("Mark")
+output_welcome_text("Western")
+output_welcome_text("Sushi")
